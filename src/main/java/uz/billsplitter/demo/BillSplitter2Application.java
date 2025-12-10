7d8bbfcae4ef6eb4ecfb1f2c53bb378df738e383
Yuriy <ukim3817@gmail.com>
2025-12-10 16:54:49 +0500

    init

---
+++ src/main/java/uz/billsplitter/demo/BillSplitter2Application.java
@@ -0,0 +1,13 @@
+package uz.billsplitter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSplitter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSplitter2Application.class, args);
+	}
+
+}
---
+++ src/test/java/uz/billsplitter/demo/BillSplitter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.billsplitter.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSplitter2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
